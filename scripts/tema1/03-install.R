ca3b104e3780ae8b2b8574b75502e4dbe530ff87
Carlos Meneses <carlosahz21@gmail.com>
2020-02-17 20:58:17 -0500

    funcion Operaciones

--- scripts/tema1/03-install.R
+++ scripts/tema1/03-install.R
@@ -9,3 +9,24 @@ library(magic)
 magic(6)
 
 installed.packages()
+
+
+operaciones <- function(x,y,num_op) {
+  
+  if (num_op == 1) {
+    x+y #suma
+  } else if (num_op == 2) {
+    x-y #resta
+  } else if (num_op == 3) {
+    x*y #multiplicacion
+  } else if (num_op == 4) {
+    if (y != 0) {
+      x/y #division
+    } else {
+      0 # si y = 0
+    }
+  } else {
+    0 # En cualquier otro caso
+  }
+  
+}
