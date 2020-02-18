244ed14dd426721d25f1f3cb8a786de63067cb6c
Carlos Meneses <carlosahz21@gmail.com>
2020-02-18 02:28:02 -0500

    ejemplo lapply

--- scripts/tema1/EjemploFuncionesVectorizadas.r
+++ scripts/tema1/EjemploFuncionesVectorizadas.r
@@ -5,3 +5,11 @@ set.seed(100)
 X <- matrix(rnorm(30),nrow = 5, ncol = 6)
 print(X)
 apply(X,1,sum) #suma de todas las filas de la matriz
+#Ejemplo de la funcion Lapply
+A <- matrix(1:9,3,3)
+B <- matrix(4:15,4,3)
+C <- matrix(8:10,3,2)
+mi_lista <- list(A,B,C)
+#ejemplo de la extraccion de todos los elementos de la segunda columna
+# de las matrices en la lista
+lapply(mi_lista, "[", , 2)
