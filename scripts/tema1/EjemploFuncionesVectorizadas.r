e9c698ced4b53a0f31aa986f39159f9acbcd0969
Carlos Meneses <carlosahz21@gmail.com>
2020-02-18 02:43:47 -0500

    ejemplo funcion Sapply

--- scripts/tema1/EjemploFuncionesVectorizadas.r
+++ scripts/tema1/EjemploFuncionesVectorizadas.r
@@ -4,11 +4,21 @@ set.seed(100)
 #con media = 0 y varianza = 1
 X <- matrix(rnorm(30),nrow = 5, ncol = 6)
 print(X)
-apply(X,1,sum) #suma de todas las filas de la matriz
+#suma de todas las filas de la matriz
+apply(X, 1, sum)
+#media de todas las columnas de la matriz
+apply(X, 2, mean)
+
 #Ejemplo de la funcion Lapply
 A <- matrix(1:9,3,3)
 B <- matrix(4:15,4,3)
 C <- matrix(8:10,3,2)
 mi_lista <- list(A,B,C)
-#Extraccion de todos los elementos de la segunda columna de las matrices en la lista
+#Extraccion de la segunda columna de cada matriz en la lista
 lapply(mi_lista, "[", , 2)
+#Extraccion de la primera fila de cada matriz en la lista
+lapply(mi_lista, "[", 1, )
+
+#Ejemplo de la funcion Sapply
+lapply(mi_lista, "[", 2, 1)
+sapply(mi_lista, "[", 2, 1)
