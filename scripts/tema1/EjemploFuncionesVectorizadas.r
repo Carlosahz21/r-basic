347d951fa042298968ba4d4b4aa07aed597ebf06
Carlos Meneses <carlosahz21@gmail.com>
2020-02-18 05:35:38 -0500

    ejemplo Mapply

--- scripts/tema1/EjemploFuncionesVectorizadas.r
+++ scripts/tema1/EjemploFuncionesVectorizadas.r
@@ -22,3 +22,7 @@ lapply(mi_lista, "[", 1, )
 #Ejemplo de la funcion Sapply
 lapply(mi_lista, "[", 2, 1)
 sapply(mi_lista, "[", 2, 1)
+
+#Ejemplo de la funcion Mapply
+Q <- matrix(c(rep(1,4),rep(2,4),rep(3,4),rep(4,4)),4,4)
+mapply(rep, 1:4, 4)
